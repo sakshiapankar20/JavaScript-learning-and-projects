d8ae4a3b6d43ed460790f7e17887f8840a318e82
Sakshi Ganesh Apankar <142079744+sakshiapankar20@users.noreply.github.com>
2025-11-04 06:02:18 +0000

    js_basics

---
+++ VarDataType/script.js
@@ -0,0 +1,11 @@
+console.log("hey hello again after a lot of days");
+
+console.log("yes i think i love javascrit than other coding languages");
+
+const heading=document.getElementById("heading").innerHTML="Sakshi Apanakr|Software Engineer";
+
+
+name="Sakshi";
+let age=23;
+console.log(name);
+console.log(age);
